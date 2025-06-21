12a8661350b3b24b47029c45a9390c18f5758ce8
David Hauser <hauser-david@hotmail.com>
2025-06-21 10:17:55 +0200

    move dataset to correct path

--- gtzan_dataset.py
+++ gtzan_dataset.py
@@ -1,6 +1,17 @@
 import kagglehub
+import shutil
+import os
 
-# Download latest version
+# Download the dataset
 path = kagglehub.dataset_download("andradaolteanu/gtzan-dataset-music-genre-classification")
 
-print("Path to dataset files:", path)
+# Define the destination path
+destination_path = "gtzan_dataset"
+
+# Create the destination directory if it doesn't exist
+os.makedirs(destination_path, exist_ok=True)
+
+# Move the dataset to the desired location
+shutil.move(path, destination_path)
+
+print("Dataset moved to:", destination_path)
